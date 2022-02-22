d44f3be7234a56264be03d51237a8578d2c9b5f3
AadityaCholayil <cholayilaaditya@gmail.com>
2022-02-22 18:20:30 +0530

    changed package name to com.mates.breathe

--- android/app/src/main/kotlin/com/example/breathe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.breathe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,6 @@ import 'package:breathe/views/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
